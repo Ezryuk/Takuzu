2a62eb74bb0592890b673e0d2e5e4e8e85679f2d
Quentin Derambure <qderambure@ecole.ensicaen.fr>
2020-03-31 11:12:31 +0200

    Add qrc

--- ModelTakuzu.cpp
+++ ModelTakuzu.cpp
@@ -46,7 +46,7 @@ ModelTakuzu::~ModelTakuzu()
 
 void ModelTakuzu::loadFile(const QString &name)
 {
-    QFile file(name);
+    QFile file("qrc:/resources/"+name);
     if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QByteArray line;
 
